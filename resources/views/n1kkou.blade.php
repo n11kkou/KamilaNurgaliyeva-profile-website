fde42dfcc5cd7ad97e6952481b4c9fae362acfd9
n11kkou <nurgalievakamila7@gmail.com>
2021-02-20 21:47:23 +0600

    first page done

--- resources/views/n1kkou.blade.php
+++ resources/views/n1kkou.blade.php
@@ -4,13 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     
     <link rel="stylesheet" href="{{ asset('css/n1kkou.css') }}">
 
     <title>N1KKOU's cv</title>
 </head>
 <body>
+    <div class="full">
 
+        <div class="menu">
+            <a href="" id="home">Home</a>
+            <a href="" id="skills">Skills</a>
+            <a href="" id="education">Education</a>
+        </div>
+
+        <div class="center">
+            <div class="photo">
+                <img src="https://p.favim.com/orig/2019/01/14/rainbow-girl-aesthetic-Favim.com-6806473.jpg" alt="" id="photo">
+            </div>
+            <div class="description">
+                <div class="text">
+                    <h1>Hi,<br>I'm <spann style="color: rgb(255, 255, 255); background-color: rgb(39, 51, 53);"> Kamila </spann><br>Web Designer</h1>
+                </div>
+                <div class="btn">
+                    <button type="button" class="btn btn-outline-dark" >Contact Me </button>
+                </div>
+                
+            
+            </div>
+        </div>
+
+
+    </div>
 
 
 
