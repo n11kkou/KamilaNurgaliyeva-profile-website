b31120870d6d12e9a3f926e8d7c5a4fd72b62eb6
n11kkou <nurgalievakamila7@gmail.com>
2021-03-19 17:31:28 +0600

    create 2 functions in api controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,5 +7,19 @@ use App\Models\Post;
 
 class ApiController extends Controller
 {
-    
+    public function index(Request $request){
+        $posts = Post::all();
+
+        return response($posts, 200);
+
+    }
+
+    public function get_post(Request $request){
+        $posts = Post::find($request->post_id);
+
+        if($posts == null)
+        return response(['message' => 'post not found'], 404);
+
+        return response($posts, 200);
+    }
 }
