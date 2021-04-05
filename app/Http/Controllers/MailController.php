3c8b15591ed57303b89a88e15b6f3f4c55bde868
n11kkou <nurgalievakamila7@gmail.com>
2021-04-05 15:57:35 +0600

    create MailController with send function

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\DemoEmail;
+
+class MailController extends Controller
+{
+    public function send(){
+        $objDemo = new \stdClass();
+        $objDemo -> sender = 'Kamila Nurgaliyeva';
+        $objDemo -> receiver = 'Kamila Nurgaliyeva';
+
+        Mail::to("nurgalievakamila7@gmail.com") -> send(new DemoEmail($objDemo));
+
+    }
+}
